87a5dd579c7a832310649f54336edaa2cb6770da
MiguelVRRL <miguelvacarios@gmail.com>
2025-11-13 10:22:33 -0400

    setup: estructura básica de graficos añadida

---
+++ graficos/graficos_dispersion.py
@@ -0,0 +1,6 @@
+class GraficoDispersion:
+    def __init__(self) -> None:
+        self.__bandas_confianza: bool = True 
+        self.__recta__estimacion: bool = True
+
+    
---
+++ graficos/graficos_doble_entrada.py
@@ -0,0 +1,2 @@
+class GraficoDobleEntrada:
+    pass
